3f29bb4edffaf439351253a2d62f474066ed966e
Karol Koziński <larzuck@gmail.com>
2025-03-10 01:02:07 +0100

    modified:   classes.py 	modified:   readme.txt

--- classes.py
+++ classes.py
@@ -1,46 +1,48 @@
 from abc import ABC, abstractmethod
 
-
-class Statek(ABC):  # Klasa abstrakcyjna
-    def __init__(self, maksymalnaZaloga, maksymalneDziala, ladownosc, predkosc, curZaloga, curDziala, curLadunek):
-        self.maksymalnaZaloga = maksymalnaZaloga
-        self.maksymalneDziala = maksymalneDziala
-        self.ladownosc = ladownosc
+class Statek(ABC):
+    def __init__(self, maxZaloga, maxDziala, maxLadunek, predkosc, curZaloga, curDziala, curLadunek):
+        self.maxZaloga = maxZaloga
+        self.maxDziala = maxDziala
+        self.maxLadunek = maxLadunek
         self.predkosc = predkosc
-        self.curZaloga = curZaloga
-        self.curDziala = curDziala
-        self.curLadunek = curLadunek
-    
-    @abstractmethod
-    def zmien_zaloge(self, oIle):
-        self.curZaloga = self.curZaloga + oIle
-        if self.curZaloga > self.maksymalnaZaloga:
-            self.curZaloga = self.maksymalnaZaloga
-        if self.curZaloga < 0:
-            self.curZaloga  = 0
-
-        
-    @abstractmethod
-    def pokaz_info(self):
-        print ('Załoga: ' + str(self.curZaloga) + '/' + str(self.maksymalnaZaloga))
-        print ('Działa: ' + str(self.curDziala) + '/' + str(self.maksymalneDziala))
-        print ('Ładowność: ' + str(self.curLadunek) + '/' + str(self.ladownosc))
-        print ('Nazwa klasy statku: ' + __name__)
-                
-
-
-
-class Fregata(Statek):  # Klasa dziedzicząca po abstrakcyjnej klasie Statek
-    def __init__(self, curZaloga, curDziala, curLadunek):
-        super().__init__(200, 24, 120, 9, curZaloga, curDziala, curLadunek)
         self.zaloga = curZaloga
         self.dziala = curDziala
         self.ladunek = curLadunek
+
+    @abstractmethod
     def zmien_zaloge(self, oIle):
-        super().zmien_zaloge()
+        self.zaloga = self.zaloga + oIle
+        if self.zaloga > self.maxZaloga:
+            self.zaloga = self.maxZaloga
+        if self.zaloga < 0:
+            self.zaloga  = 0
+
+
+
+            
     def pokaz_info(self):
-        super().pokaz_info()
+        return f"Typ statku: {self.__class__.__name__}\n" \
+               f"Maksymalna załoga: {self.maxZaloga}, Obecna załoga: {self.zaloga}\n" \
+               f"Maksymalne działa: {self.maxDziala}, Obecne działa: {self.dziala}\n" \
+               f"Maksymalny ładunek: {self.maxLadunek}, Obecny ładunek: {self.ladunek}\n" \
+               f"Prędkość: {self.predkosc} węzłów"
 
+class Fregata(Statek):  
+    def __init__(self, curZaloga, curDziala, curLadunek):
+        super().__init__(200, 24, 120, 9, curZaloga, curDziala, curLadunek)
 
-SantaMaria = Fregata(50,20,10)
-SantaMaria.pokaz_info()
+    def zmien_zaloge(self, oIle):
+        super().zmien_zaloge(oIle)
+
+    def pokaz_info(self):
+        return super().pokaz_info()
+
+# Tworzenie obiektu klasy Fregata
+fregata = Fregata(150, 20, 100)
+
+# Wywołanie metody pokaz_info()
+print(fregata.pokaz_info())
+
+fregata.zmien_zaloge(-15)
+print(fregata.pokaz_info())
